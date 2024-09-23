31031c17aa14bff6c6cb51ef6dead1c804627f20
Natalia Saveleva <saveleva_ni@ascon.ru>
2024-09-23 12:36:59 +0400

    Lesson 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Миникалькулятор в деле. Выберите режим: ");
+            Console.WriteLine("1: сложение");
+            Console.WriteLine("2: вычитание");
+            Console.WriteLine("3: умножение");
+            Console.WriteLine("4: деление");
+            int type = Convert.ToInt32(Console.ReadLine());
+            decimal Result = 0;
+
+            switch (type) 
+                {
+                case 1:
+                    {
+                        Console.Write("Введите 1 слагаемое: ");
+                        decimal answer = Convert.ToDecimal(Console.ReadLine());
+                        Result += answer;
+                        Console.Write("Введите 2 слагаемое: ");
+                        answer = Convert.ToDecimal(Console.ReadLine());
+                        Result += answer;
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.Write("Введите уменьшаемое: ");
+                        decimal answer = Convert.ToDecimal(Console.ReadLine());
+                        Result = answer;
+                        Console.Write("Введите вычитаемое: ");
+                        answer = Convert.ToDecimal(Console.ReadLine());
+                        Result -= answer;
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Write("Введите 1 множитель: ");
+                        decimal answer = Convert.ToDecimal(Console.ReadLine());
+                        Result = answer;
+                        Console.Write("Введите 2 множитель: ");
+                        answer = Convert.ToDecimal(Console.ReadLine());
+                        Result *= answer;
+                        break;
+
+                    }
+                case 4:
+                    {
+                        Console.Write("Введите делимое: ");
+                        decimal answer = Convert.ToDecimal(Console.ReadLine());
+                        Result = answer;
+                        Console.Write("Введите делитель: ");
+                        answer = Convert.ToDecimal(Console.ReadLine());
+                        Result /= answer;
+                        break;
+
+                    }
+                default: {
+                        Console.WriteLine("Я такое не умею, запустите еще раз пожалуйста");
+                        break;
+                    }
+            }
+
+            Console.WriteLine($"Результат: {Result}");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName;
+            String LastName;
+            String SurName;
+            Console.WriteLine("Представьтесь, пожалуйста");
+
+            Console.Write("Фамилия: ");
+            LastName = Console.ReadLine();
+
+            Console.Write("Имя: ");
+            FirstName = Console.ReadLine();
+
+            Console.Write("Отчество: ");
+            SurName = Console.ReadLine();
+
+            Console.WriteLine($"Введены данные: {LastName} {FirstName} {SurName}") ;
+            Console.ReadLine();
+        }
+    }
+}
