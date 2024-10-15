5655c34b86b5fa7b48002303ef2242c772690d8b
Natalia Saveleva <saveleva_ni@ascon.ru>
2024-10-15 16:22:31 +0400

    Simple number

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите порядковый номер простого числа: ");
+        int n = 0;
+        try
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+
+        int count = 0;
+        int num = 2;
+
+        while (count < n)
+        {
+            if (IsSimple(num))
+            {
+                count++;
+            }
+            num++;
+        }
+
+        Console.WriteLine($"Простое число под номером {n} : {num - 1}");
+    }
+
+    static bool IsSimple(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
